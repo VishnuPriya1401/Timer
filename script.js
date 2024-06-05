b68260ccf38438b6d559d0e1df193b58f05c7a9c
VishnuPriya1401 <145310382+VishnuPriya1401@users.noreply.github.com>
2024-06-05 15:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+let isPause = false;
+let isReset = false;
+let int = null;
+
+let hr = 0;
+let min = 0;
+let sec = 0;
+let time = 0;
+
+let hoursUnit = 0;
+let minutesUnit = 0;
+let secondsUnit = 0;
+
+let hours = '';
+let minutes = '';
+let seconds = '';
+
+let displayTimerSet = document.querySelector('.display-timer');
+
+document.querySelector('.js-start-btn').addEventListener('click', ()=> { 
+  setTime();
+  document.querySelector('.set-timer').style.display='none';
+  document.querySelector('.run-timer').style.display='block';
+  
+});
+
+document.querySelector('.set-timer-btn').addEventListener('click',()=>{
+  clearInterval(int);
+  document.querySelector('.set-timer').style.display='block';
+  document.querySelector('.run-timer').style.display='none';
+  displayTimerSet.innerHTML='';
+})
+
+document.querySelector('.pause-play-btn').addEventListener('click',()=>{
+  if(!isPause){
+    isPause = true;
+    clearInterval(int);
+    document.querySelector('.pause-play-btn').innerHTML = `&#9658;`;
+  }
+  else{
+    isPause = false;
+    document.querySelector('.pause-play-btn').innerHTML = `&#10073; &#10073;`;
+    int = setInterval(displayTimer, 1000);
+  }
+});
+
+document.querySelector('.reset-btn').addEventListener('click', ()=>{
+  isPause = true;
+  clearInterval(int);
+  document.querySelector('.pause-play-btn').innerHTML = `&#9658;`;
+
+  time = (hr*60*60) + (min*60) + sec;
+  displayTimer();
+})
+
+function setTime(){
+  
+  const hrInput = document.querySelector('.js-hr');
+  hr = Number(hrInput.value);
+
+  const minInput = document.querySelector('.js-min');
+  min = Number(minInput.value);
+
+  const secInput = document.querySelector('.js-sec');
+  sec = Number(secInput.value);
+  
+  hrInput.value='';
+  secInput.value='';
+  minInput.value='';
+
+  time = (hr*60*60) + (min*60) + sec;
+
+  int = setInterval(displayTimer, 1000);
+}
+
+function displayTimer(){
+  
+  if(time){
+    
+    hoursUnit = Math.floor(time/(60*60));
+    minutesUnit = Math.floor((time%(60*60))/60);
+    secondsUnit = time%60;
+
+    hours = hoursUnit<10? '0'+hoursUnit : hoursUnit;
+    minutes = minutesUnit<10? '0'+minutesUnit : minutesUnit;
+    seconds = secondsUnit<10? '0'+secondsUnit : secondsUnit;
+
+    displayTimerSet.innerHTML=`${hours} : ${minutes} : ${seconds}`;
+  }
+  time--;
+}
